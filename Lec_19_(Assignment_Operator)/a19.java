a3c668ef18dd390bb27e218e664069654e62a674
Anshul2202 <anshul220206@gmail.com>
2026-02-04 00:29:05 +0530

    Pushing a new file

---
+++ Lec_19_(Assignment_Operator)/a19.java
@@ -0,0 +1,17 @@
+public class a19 {
+
+    public static void main(String arr[]) {
+
+        // ------- Assignment Operator --------
+
+        int x = 5;
+        // here '=' is the assigment operator
+        // which assigns the value 5 to the variable 'x'
+
+        // -------- Equal To Operator ----------
+
+        if (x == 5) {
+            System.out.println("That's the difference between Assignment and equal to operator");
+        }
+    }
+}
