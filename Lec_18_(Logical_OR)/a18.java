c680a768b6d0aa184af664d3102bdbc975e6bb35
Anshul2202 <anshul220206@gmail.com>
2026-02-04 00:17:00 +0530

    Pushing a new java file

---
+++ Lec_18_(Logical_OR)/a18.java
@@ -0,0 +1,22 @@
+public class a18 {
+
+    public static void main(String arr[]) {
+
+        // ---------- Logical OR ------------
+
+        int a = 18;
+        int b = 100;
+
+        if (a >= 18 || b <= 10) {
+            // Here first condition is true
+            // And second condition is false
+
+            System.out.println("Logical OR is working");
+
+            // Logical OR will give true as an output
+            // If atleast one of the condition is true
+            // Only if both the conditions are false
+            // It will give false as output
+        }
+    }
+}
