627244b042293cb4d14ddc1a93b7224c3c7ac98b
Anshul2202 <anshul220206@gmail.com>
2026-02-04 20:05:21 +0530

    Pushing a new file

---
+++ Lec_20_(Ternary_Operator)/a20.java
@@ -0,0 +1,14 @@
+public class a20 {
+
+    public static void main(String arr[]) {
+
+        // ------- Ternary Operator ----------
+
+        int x = 10;
+
+        boolean condition = x == 5 ? true : false;
+
+        System.out.println(condition);
+
+    }
+}
