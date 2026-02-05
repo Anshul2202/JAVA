1c88efbb2b5c892e8bed45c4dc146bbe20fc3ff0
Anshul2202 <anshul220206@gmail.com>
2026-02-05 19:16:02 +0530

    Pushing a new java file

---
+++ Lec_22_(Keywords_&_Expression)/a22.java
@@ -0,0 +1,20 @@
+public class a22 {
+
+    public static void main(String arr[]) {
+
+        // ------- Keywords -----------
+
+        // int int = 5;
+        // This will show an error
+        // Because int is an reserved keyword in java
+
+        int int1 = 5;
+        System.out.println(int1);
+
+        // ------- Expression -----------
+
+        int x = 10 + 5;
+        // x = 10 + 5 ----> Expression
+        System.out.println(x);
+    }
+}
