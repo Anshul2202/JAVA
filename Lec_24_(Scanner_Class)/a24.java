3288eb00267639242f0165d7a00fb52dcf0806e1
Anshul2202 <anshul220206@gmail.com>
2026-02-05 23:29:11 +0530

    Pushing a new java file

---
+++ Lec_24_(Scanner_Class)/a24.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class a24 {
+
+    public static void main(String arr[]) {
+
+        // ------- Scanner Class --------
+        //
+        // Used to take input from user
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.next();
+        System.out.println(name);
+        sc.nextLine();
+
+        String fullName = sc.nextLine();
+        System.out.println(fullName);
+
+        int age = sc.nextInt();
+        System.out.println(age);
+
+    }
+}
