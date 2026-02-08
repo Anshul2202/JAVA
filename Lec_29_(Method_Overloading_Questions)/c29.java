dde2a93a81fc3a0f1a4b1670561d4c005c376b22
Anshul2202 <anshul220206@gmail.com>
2026-02-08 23:07:31 +0530

    Made some changes

--- Lec_29_(Method_Overloading_Questions)/c29.java
+++ Lec_29_(Method_Overloading_Questions)/c29.java
@@ -3,9 +3,14 @@ public class c29 {
     public static void main(String arr[]) {
 
         printYearsAndDays(525600);
+        printYearsAndDays(-1);
     }
 
     public static void printYearsAndDays(long mins) {
+        if (mins < 0) {
+            System.out.println("Invalid Input");
+            return;
+        }
         long hr = mins / 60;
         long days = hr % 24;
         long tempDays = hr / 24;
