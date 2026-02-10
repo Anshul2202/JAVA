1a9ee15a578fec7dc7fb04a9f001bd3e9316180c
Anshul2202 <anshul220206@gmail.com>
2026-02-10 22:32:36 +0530

    Pushing a new java file

---
+++ Lec_30_(If-Statement)/a30.java
@@ -0,0 +1,19 @@
+public class a30 {
+
+    public static void main(String arr[]) {
+
+        // ----------- If Statement -------------
+
+        boolean isAdult = true;
+
+        if (isAdult) {
+            System.out.println("Eligible to vote......");
+        }
+
+        int runs = 250;
+
+        if (runs >= 200) {
+            System.out.println("Good Score....");
+        }
+    }
+}
