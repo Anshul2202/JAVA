53d25c8c3149a391dfd8f2f5ec8e182629f3e8bb
Anshul2202 <anshul220206@gmail.com>
2026-02-11 22:43:11 +0530

    Pushing a new java file

---
+++ Lec_33_(Switch_Questions)/a33.java
@@ -0,0 +1,28 @@
+public class a33 {
+
+    public static void main(String arr[]) {
+
+        char val = 'x';
+
+        switch (val) {
+
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println("Is vowel");
+                break;
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println("Is vowel");
+                break;
+            default:
+                System.out.println("Not a vowel");
+
+        }
+    }
+}
