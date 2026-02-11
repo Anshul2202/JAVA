1aaf05852a5f36c275869312af6746a06391ed9e
Anshul2202 <anshul220206@gmail.com>
2026-02-11 22:45:43 +0530

    Made some changes

--- Lec_33_(Switch_Questions)/a33.java
+++ Lec_33_(Switch_Questions)/a33.java
@@ -2,22 +2,15 @@ public class a33 {
 
     public static void main(String arr[]) {
 
-        char val = 'x';
+        char val = 'a';
 
-        switch (val) {
+        switch (Character.toUpperCase(val)) {
 
             case 'A':
             case 'E':
             case 'I':
             case 'O':
             case 'U':
-                System.out.println("Is vowel");
-                break;
-            case 'a':
-            case 'e':
-            case 'i':
-            case 'o':
-            case 'u':
                 System.out.println("Is vowel");
                 break;
             default:
