c472a3a439c7c8ca7e5b0435c747838daefa9e45
Anshul2202 <anshul220206@gmail.com>
2026-02-12 22:25:40 +0530

    Pushing a new java file

---
+++ Lec_34_(For_Loop)/a34.java
@@ -0,0 +1,11 @@
+public class a34 {
+
+    public static void main(String arr[]) {
+
+        // ---------- For Loop -------------
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
