330d4fdc964d2353e4e1adfa74681b0dc8e0aa8f
Anshul2202 <anshul220206@gmail.com>
2026-02-13 22:44:10 +0530

    Changed file name

--- Lec_35_(For_Loop_Questions)/c35.java
+++ Lec_35_(For_Loop_Questions)/c35.java
@@ -1,10 +1,31 @@
+import java.util.*;
+
 public class c35 {
 
     public static void main(String arr[]) {
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
     }
 
-    public static void printPrime(int n) {
+    public static boolean isPrime(int x) {
 
+        for (int i = 2; i < x; i++) {
+
+            if (x % i == 0) {
+                return false;
+            }
+
+        }
+        return true;
     }
+
 }
