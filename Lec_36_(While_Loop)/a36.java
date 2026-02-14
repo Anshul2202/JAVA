6f4f22842e869173132a89a52001216259901e73
Anshul2202 <anshul220206@gmail.com>
2026-02-14 21:59:18 +0530

    Pushing a new java file

---
+++ Lec_36_(While_Loop)/a36.java
@@ -0,0 +1,23 @@
+public class a36 {
+
+    public static void main(String arr[]) {
+
+        // -------- While Loop -------------
+
+        int count = 1;
+
+        while (count != 10) {
+            System.out.println(count);
+            count++;
+        }
+
+        // ------- Do While Loop -------------
+
+        int n = 1;
+
+        do {
+            System.out.println(n);
+            n++;
+        } while (n <= 10);
+    }
+}
