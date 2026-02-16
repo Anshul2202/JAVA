3ec8f506aa39ceab46ffaa0bcc46bb9dd40b5959
Anshul2202 <anshul220206@gmail.com>
2026-02-16 22:38:07 +0530

    Solved a new question

---
+++ Lec_39_(Questions)/b39.java
@@ -0,0 +1,30 @@
+public class b39 {
+
+    public static void main(String arr[]) {
+        sumFirstAndLast(252);
+
+    }
+
+    public static void sumFirstAndLast(int n) {
+
+        if (n < 0) {
+            System.out.println("Invalid input!");
+            return;
+        }
+
+        int temp = n;
+        int lastDigit = temp % 10;
+        int firstDigit = 0;
+
+        while (true) {
+            temp /= 10;
+            if (temp != 0) {
+                firstDigit = temp;
+            } else {
+                break;
+            }
+        }
+
+        System.out.println("The sum of first & last digit : " + (firstDigit + lastDigit));
+    }
+}
