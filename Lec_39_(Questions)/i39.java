0ca4d2d3970ba994f2e3d073bfbc151a71338301
Anshul2202 <anshul220206@gmail.com>
2026-02-19 00:29:42 +0530

    Solved a new question

---
+++ Lec_39_(Questions)/i39.java
@@ -0,0 +1,25 @@
+public class i39 {
+
+    public static void main(String arr[]) {
+
+        int result = getBucketCount(4, 2, 1, 2);
+        System.out.println(result);
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extra) {
+
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
+            System.out.println("Invalid input!");
+            return -1;
+        }
+
+        double areaOfWall = width * height;
+
+        int totalBucket = (int) (areaOfWall / areaPerBucket);
+
+        int buyBucket = totalBucket - extra;
+
+        return buyBucket;
+
+    }
+}
