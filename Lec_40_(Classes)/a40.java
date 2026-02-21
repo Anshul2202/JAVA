218ecb94c08c7d0b32dd912173eb1098fd169029
Anshul2202 <anshul220206@gmail.com>
2026-02-21 15:56:33 +0530

    Made some changes

--- Lec_40_(Classes)/a40.java
+++ Lec_40_(Classes)/a40.java
@@ -2,11 +2,17 @@ public class a40 {
 
     public static void main(String arr[]) {
 
+        // Creating object of class cellPhone
         cellPhone Apple = new cellPhone();
-        Apple.model = "12 Pro Max";
-        Apple.price = 80000;
-        Apple.ram = 8;
 
-        System.out.println(Apple.model);
+        // Set methods
+        Apple.setModel("12 PRO MAX");
+        Apple.setPrice(80000);
+        Apple.setRam(8);
+
+        // Get methods
+        Apple.getModel();
+        Apple.getPrice();
+        Apple.getRam();
     }
 }
--- Lec_40_(Classes)/cellPhone.java
+++ Lec_40_(Classes)/cellPhone.java
@@ -1,7 +1,35 @@
 
 public class cellPhone {
 
-    public String model;
-    public int price;
-    public int ram;
+    private String model;
+    private int price;
+    private int ram;
+
+    // Set method
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    // Get methods
+
+    public void getModel() {
+        System.out.println(this.model);
+    }
+
+    public void getPrice() {
+        System.out.println(this.price);
+    }
+
+    public void getRam() {
+        System.out.println(this.ram);
+    }
 }
