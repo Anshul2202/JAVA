fbd04b01b50d8f41dbfebfff85ccc136f88301da
Anshul2202 <anshul220206@gmail.com>
2026-02-23 20:51:07 +0530

    Made some changes

--- Lec_41_(Class_Questions)/b41.java
+++ Lec_41_(Class_Questions)/b41.java
@@ -3,6 +3,7 @@ public class b41 {
     public static void main(String arr[]) {
 
         Name name1 = new Name();
+        Name name2 = new Name();
 
         name1.setFirstName("Anshul");
         name1.getFirstName();
@@ -11,5 +12,11 @@ public class b41 {
         name1.getLastName();
 
         name1.getFullName();
+
+        name2.setFirstName("Siddharth");
+        name2.getFirstName();
+        name2.setLastName("Singh");
+        name2.getLastName();
+        name2.getFullName();
     }
 }
