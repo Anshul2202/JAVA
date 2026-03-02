7a8051e6180c060f43973d913c1a98858d072721
Anshul2202 <anshul220206@gmail.com>
2026-03-02 21:24:55 +0530

    Pushing a new java file

--- Lec_42_(Constructor)/a42.java
+++ Lec_42_(Constructor)/a42.java
@@ -2,5 +2,10 @@ public class a42 {
 
     public static void main(String arr[]) {
 
+        cons c1 = new cons();
+        c1.getDetails();
+
+        cons c2 = new cons("Anshul", "9898987989", 87);
+        c2.getDetails();
     }
 }
--- Lec_42_(Constructor)/cons.java
+++ Lec_42_(Constructor)/cons.java
@@ -5,4 +5,23 @@ public class cons {
     public cons() {
         System.out.println("Default Construcutor was called...");
     }
+
+    // Constructor OverLoading.....
+
+    public cons(String name, String phoneNo, int marks) {
+        System.out.println("Defined Constructor was called....");
+        this.name = name;
+        this.phoneNo = phoneNo;
+        this.marks = marks;
+    }
+
+    private String name;
+    private String phoneNo;
+    private int marks;
+
+    public void getDetails() {
+        System.out.println(this.name);
+        System.out.println(this.phoneNo);
+        System.out.println(this.marks);
+    }
 }
