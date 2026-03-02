c1c18caa5f049745672151fbc3fb10a99a6920df
Anshul2202 <anshul220206@gmail.com>
2026-03-02 21:17:28 +0530

    Pushing a new java file

---
+++ Lec_42_(Constructor)/a42.java
@@ -0,0 +1,6 @@
+public class a42 {
+
+    public static void main(String arr[]) {
+
+    }
+}
---
+++ Lec_42_(Constructor)/cons.java
@@ -0,0 +1,8 @@
+public class cons {
+
+    // Defining constructor
+
+    public cons() {
+        System.out.println("Default Construcutor was called...");
+    }
+}
