c0c3569c97e81f284d731487ee8330ccf8e5a02a
Anshul2202 <anshul220206@gmail.com>
2026-03-06 22:10:41 +0530

    Done some testing

--- Lec_43_(Constructor_Questions)/Q3/c43.java
+++ Lec_43_(Constructor_Questions)/Q3/c43.java
@@ -4,7 +4,7 @@ public class c43 {
 
     public static void main(String arr[]) {
 
-        floor f1 = new floor(5.87, 2.762);
+        floor f1 = new floor(5.99, 3.75);
         double totalArea = f1.getArea();
         carpet c1 = new carpet(3.5);
         double costPerArea = c1.getCost();
