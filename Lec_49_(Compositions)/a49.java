f7f27707e2dec912efde1e0068c10dc9361ac5a5
Anshul2202 <anshul220206@gmail.com>
2026-03-12 23:09:26 +0530

    Made some changes

--- Lec_49_(Compositions)/a49.java
+++ Lec_49_(Compositions)/a49.java
@@ -2,7 +2,7 @@ public class a49 {
 
     public static void main(String arr[]) {
 
-        Complex c1 = new Complex("All in one complex", new Library("ABC LIBRARY", new Floor(5, 1000)));
+        Complex c1 = new Complex("All in one complex", new Library("APJ LIBRARY", new Floor(5, 1000)));
 
         c1.getLibrary().getBooks();
         c1.getLibrary().getlibFloor().cleanFloor();
