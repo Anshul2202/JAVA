97bc4e4f8dfbcb239982cfcfff543abd8a558460
Anshul2202 <anshul220206@gmail.com>
2026-03-15 23:36:14 +0530

    New topic covered

---
+++ Lec_53_(Polymorphism)/a53.java
@@ -0,0 +1,13 @@
+public class a53 {
+
+    public static void main(String arr[]) {
+
+        Animal animal = new Animal();
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+
+        animal.makeSound();
+        cat.makeSound();
+        dog.makeSound();
+    }
+}
