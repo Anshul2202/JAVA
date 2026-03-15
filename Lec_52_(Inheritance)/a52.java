1a72e8cbac7912b432fa255236494c2518d7607f
Anshul2202 <anshul220206@gmail.com>
2026-03-15 21:07:50 +0530

    Some debugging done

--- Lec_52_(Inheritance)/a52.java
+++ Lec_52_(Inheritance)/a52.java
@@ -2,7 +2,7 @@ public class a52 {
 
     public static void main(String arr[]) {
 
-        Car c1 = new Car("Fortuner", 200, "No SunRoof", "6 iches");
+        Car c1 = new Car("Fortuner", 200, "No SunRoof", "7 iches");
         BmwM5 Bmw = new BmwM5("BMW-M5", 250, "SunRoof", "1.5 inches", "2018", 5000000);
 
         c1.getDetails();
