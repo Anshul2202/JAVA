19d07f3edc2e7d71474f3264184a27095dd29bb4
Anshul2202 <anshul220206@gmail.com>
2026-04-06 23:55:52 +0530

    Made some changes

--- Lec_65_(Abstract_Class_Questions)/Q2/S1.java
+++ Lec_65_(Abstract_Class_Questions)/Q2/S1.java
@@ -2,18 +2,18 @@ package Q2;
 
 public class S1 extends Student {
 
-    String name;
+    String fullName;
     double percentage;
 
     public S1(String name, double percentage) {
-        this.name = name;
+        this.fullName = name;
         this.percentage = percentage;
     }
 
     // Implementing Abstract methods
     @Override
     public void studentName() {
-        System.out.println("The student name is : " + name);
+        System.out.println("The student name is : " + fullName);
     }
 
     @Override
