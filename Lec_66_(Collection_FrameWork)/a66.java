db873423b542cc657b00876422d1075d7f1eb6ff
Anshul2202 <anshul220206@gmail.com>
2026-04-12 00:03:43 +0530

    Learned about sorting

--- Lec_66_(Collection_FrameWork)/a66.java
+++ Lec_66_(Collection_FrameWork)/a66.java
@@ -9,19 +9,26 @@ public class a66 {
         // List
         List<Integer> list = new ArrayList();
 
-        list.add(0);
+        list.add(6);
+        list.add(3);
+        list.add(5);
+        list.add(8);
         list.add(1);
         list.add(2);
-        list.add(3);
-        list.add(4);
         System.out.println(list);
 
-        // Creating Iterator For List
+        // Sorting the List
+        Collections.sort(list);
 
+        // Creating Iterator For List
         Iterator IT = list.listIterator();
 
         while (IT.hasNext()) {
             System.out.println(IT.next());
         }
+
+        // Binary Search In The List
+        int idx = Collections.binarySearch(list, 8);
+        System.out.println("The value 8 is at the index : " + idx);
     }
 }
