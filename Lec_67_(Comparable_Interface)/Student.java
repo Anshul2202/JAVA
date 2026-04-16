d86d8c910527633ed1054ce09273b20b485394d7
Anshul2202 <anshul220206@gmail.com>
2026-04-17 02:35:33 +0530

    Learned about comparable interface

---
+++ Lec_67_(Comparable_Interface)/Student.java
@@ -0,0 +1,42 @@
+public class Student implements Comparable<Student> {
+
+    private String name;
+    private int marks;
+
+    public Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    // With this code Sorting can be done
+
+    // Sorting on the basis of name
+
+    // @Override
+    // public int compareTo(Student s) {
+
+    // if (this.name.compareTo(s.name) == 0) {
+    // return 0;
+    // } else if (this.name.compareTo(s.name) > 0) {
+    // return 1;
+    // } else {
+    // return -1;
+    // }
+
+    // }
+
+    // Sorting on the basis of marks
+
+    @Override
+    public int compareTo(Student s) {
+        return s.marks - this.marks;
+    }
+}
---
+++ Lec_67_(Comparable_Interface)/a67.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class a67 {
+
+    public static void main(String arr[]) {
+
+        ArrayList<Student> list = new ArrayList<>();
+
+        list.add(new Student("Siddharth", 97));
+        list.add(new Student("Anshul", 95));
+        list.add(new Student("Rahul", 96));
+        list.add(new Student("Shreya", 99));
+
+        Collections.sort(list);
+
+        // Accessing All the Obejcts in the list
+
+        for (Student s : list) {
+            System.out.println(s.getName() + " " + s.getMarks());
+        }
+    }
+}
