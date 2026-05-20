35432df7758aa81e01f5bd33d34e03a71efd9381
Anshul2202 <anshul220206@gmail.com>
2026-05-20 23:59:28 +0530

    Sovled question on arraylist

---
+++ Lec_70_(ArrayList_Questions)/Q1/Dates.java
@@ -0,0 +1,20 @@
+public class Dates {
+    
+    private String Name;
+    private String date;
+
+    public Dates(String Name , String date){
+        this.Name = Name;
+        this.date = date;
+
+    }
+
+
+    public String getName(){
+        return Name;
+    }
+
+    public String getDate(){
+        return date;
+    }
+}
