bb9e6974573226f6176ffff10789a45fd71466d8
Anshul2202 <anshul220206@gmail.com>
2026-05-23 05:02:21 +0530

    Learned about LinkedList

---
+++ Lec_72_(LinkedList)/a72.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class a72 {
+    
+    public static void main(String arr[]){
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        // for(int i : list){
+        //     System.out.println(i);
+        // }
+
+        Iterator<Integer> IT = list.listIterator();
+
+        while(IT.hasNext()){
+            System.out.println(IT.next());
+        }
+        System.out.println("..........................");
+
+        list.add(3 , 10);
+
+        for(int i : list){
+            System.out.println(i);
+        }
+        System.out.println("..........................");
+
+        list.remove(3);
+
+        for(int i : list){
+            System.out.println(i);
+        }
+    }
+}
