495e565a4dfa977fb2a6e817548bbcd969a0b07f
Anshul2202 <anshul220206@gmail.com>
2026-06-05 12:25:11 +0530

    Revisited Maps

--- Lec_73_(HashMaps)/a73.java
+++ Lec_73_(HashMaps)/a73.java
@@ -1,50 +1,33 @@
 import java.util.*;
 
-public class a73{
+public class a73 {
 
-    public static void main(String arr[]){
-        
-        Map<String , String> company = new HashMap<>();
- 
+    public static void main(String arr[]) {
+
+        Map<Integer, String> student = new HashMap<>();
+
         // put
-        company.put("Founder" , "Anshul");
-        company.put("Co-Founder" , "Siddharth");
-        company.put("HR" , "Ayush");
-        company.put("Manager" , "Rahul");
+        student.put(1, "Anshul");
+        student.put(2, "Siddharth");
+        student.put(3, "Rahul");
+        student.put(4, "Ayush");
+        student.put(5, "Aman");
 
-        // for(String key : company.keySet()){
-        //     System.out.println(key + " - " + company.get(key));
+        // for(int key : student.keySet()){
+        // System.out.println(key + " - " + student.get(key));
         // }
 
         // remove
-        company.remove("Manager");
+        student.remove(4);
 
-        // for(String key : company.keySet()){
-        //     System.out.println(key + " - " + company.get(key));
+        // for (int key : student.keySet()) {
+        // System.out.println(key + " - " + student.get(key));
         // }
 
-        
         // replace
-        company.replace("HR" , "Rahul");
-
-        // for(String key : company.keySet()){
-        //     System.out.println(key + " - " + company.get(key));
-        // }
-
-        company.replace("HR" , "Rahul" , "Ayush");
-
-        // for(String key : company.keySet()){
-        //     System.out.println(key + " - " + company.get(key));
-        // }
-
-        // -------------------------------------------------------------------------- // 
-
-        company.put("Manager" , "Rahul");
-
-        for(String key : company.keySet()){
-            System.out.println(key + " - " + company.get(key));
+        student.replace(5, "Aman" , "Ayush");
+        for (int key : student.keySet()) {
+            System.out.println(key + " - " + student.get(key));
         }
-
-
     }
 }
